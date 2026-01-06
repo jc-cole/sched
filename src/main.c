e749c531117a8d00e79ce92de5059d1250765984
jc-cole <jaredcole@umass.edu>
2026-01-05 19:31:14 -0500

    half implement timerfd + signalfd round robin polling loop

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,6 +10,11 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <sys/timerfd.h>
+#include <sys/signalfd.h>
+#include <poll.h>
+#include <time.h>
+#include <unistd.h>
 
 #define MAX_PROCESSES 256
 #define MAX_LINE_SIZE 256
@@ -22,14 +28,15 @@ typedef struct ProcessNode {
     int argc;
     pid_t pid;
     struct ProcessNode *next;
-    struct launcProcessNode *prev;
+    struct ProcessNode *prev;
     ProcessState state;
     int exit_code;
+    int terminating_signal;
 } ProcessNode;
 
 
-void free_parse_result(ProcessNode parse_result) {
-    free(parse_result.argv);
+void free_process_node(ProcessNode process_node) {
+    free(process_node.argv);
 }
 
 void log_processes(ProcessNode processes[], int cmds_count) {
@@ -40,6 +47,60 @@ void log_processes(ProcessNode processes[], int cmds_count) {
         printf("argc: %d\n", processes[i].argc);
         printf("\n");
     }
+}
+
+ProcessNode *get_process_node(pid_t pid, ProcessNode processes[], int cmds_count) {
+    for (int i = 0; i < cmds_count; i++) {
+        if (processes[i].pid == pid) {
+            return &processes[i];
+        }
+    }
+    return NULL;
+}
+
+int handle_child_exit(ProcessNode *process_node, int exit_code) {
+    process_node->exit_code = exit_code;
+    process_node->terminating_signal = -1;
+    process_node->state = DONE;
+}
+
+int handle_child_term(ProcessNode *process_node, int signal_num) {
+    process_node->exit_code = -1;
+    process_node->terminating_signal = signal_num;
+    process_node->state = DONE;
+}
+
+int make_timer_fd(int quantum_ms) {
+    int tfd = timerfd_create(CLOCK_MONOTONIC, 0);
+
+    struct itimerspec ts = {0};
+
+    ts.it_value.tv_nsec    = quantum_ms * 1000 * 1000;
+    ts.it_interval.tv_nsec = quantum_ms * 1000 * 1000;
+
+    if (timerfd_settime(tfd, 0, &ts, NULL) == -1) {
+        perror("timerfd_settime");
+    }
+
+    return tfd;
+}
+
+int make_sigchld_fd() {
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+
+    if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1) {
+        perror("sigprocmask");
+        return 1;
+    }
+
+    int sfd = signalfd(-1, &mask, 0);
+    if (sfd == -1) {
+        perror("signalfd");
+        return -1;
+    }
+
 }
 
 int launch_tasks(ProcessNode processes[], int cmds_count) {
@@ -85,6 +146,91 @@ int launch_tasks(ProcessNode processes[], int cmds_count) {
     }
     return 0;
 }
+
+int dequeue_node(ProcessNode node) {
+    if (node.next == &node) { //if node is pointing to self
+        free_process_node(node);
+        return -1;
+    }
+    node.prev->next = node.next;    if (fork() == 0) {
+        
+    }
+    free_process_node(node);
+    return 0;
+}
+
+int round_robin(ProcessNode processes[], int cmds_count, int quantum_ms) {
+    ProcessNode *current_node = NULL;
+
+    int active_jobs = cmds_count;
+    for (int i = 0; i < cmds_count; i++) {
+        if (processes[i].state == DONE || processes[i].state == EXECFAILURE) {
+            if (dequeue_node(processes[i]) == -1) {
+                return 0;
+            }
+            active_jobs--;
+        } else if (current_node == NULL) {
+            current_node = &processes[i];
+        }
+    }
+
+    struct pollfd pfds[2];
+
+    pfds[0].fd = make_timer_fd(quantum_ms);
+    pfds[0].events = POLLIN;
+
+    pfds[1].fd = make_sigchld_fd();
+    pfds[1].events = POLLIN;
+
+    while (active_jobs > 0) {
+        // next: implement poll + timerfd + signalfd to implement waiting for quantam and signal handling
+        printf("%s\n", current_node->path_to_executable);
+
+        kill(current_node->pid, SIGCONT);
+
+        if (poll(pfds, 2, -1) == -1) {
+            exit(2);
+        }
+
+        if (pfds[0].revents & POLLIN) {
+            // quantum expired
+            printf("the quantum expired\n");
+            kill(current_node->pid, SIGSTOP);
+            current_node = current_node->next;
+            continue;
+        }
+
+        if (pfds[1].revents & POLLIN) {
+            // child exited / stopped
+            pid_t pid;
+            int st;
+            bool current_process_done = false;
+            while ((pid = waitpid(-1, &st, WNOHANG | WUNTRACED | WCONTINUED)) > 0) {
+                // WIFEXITED / WIFSIGNALED / WIFSTOPPED / WIFCONTINUED
+                if (WIFEXITED(st)) {
+                    handle_child_exit(get_process_node(pid, processes, cmds_count), WEXITSTATUS(st));
+                    if (pid == current_node->pid) {
+                        current_process_done = true;
+                    }
+                } else if (WIFSIGNALED(st)) {
+                    handle_child_term(get_process_node(pid, processes, cmds_count), WTERMSIG(st));
+                    if (pid == current_node->pid) {
+                        current_process_done = true;
+                    }
+                }
+            }
+            if (current_process_done) {
+                continue;
+            }
+
+        }
+    }
+
+    printf("All jobs complete ");
+    log_processes(processes, cmds_count);
+
+}
+
 
 
 
@@ -158,7 +304,7 @@ ProcessNode parse_line(char *line) {
 
 int main(int argc, char *argv[]) {
     char* scheduler_policy = NULL;
-    int quantum = 100;
+    int quantum = 1000;
     char* workload_filename = NULL;
     if (argc > 0) {
         for (int i = 1; i < argc; i++) {
@@ -203,7 +349,7 @@ int main(int argc, char *argv[]) {
 
     launch_tasks(processes, cmds_count);
 
-    log_processes(processes, cmds_count);
+    round_robin(processes, cmds_count, quantum);
     
     return 0;
 }
